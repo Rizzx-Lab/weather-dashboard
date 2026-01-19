cdea2c8432ef2ca30ffcf228474a1e89c22a1813
Rizx-Lab <muhammadfarizsetiawan1604@gmail.com>
2026-01-19 13:37:16 +0700

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// SEMENTARA disable PWA plugin untuk hindari error
 export default defineConfig({
-  plugins: [
-    react()
-    // VitePWA plugin DISABLED sementara
-    // VitePWA({ ... })
-  ],
+  plugins: [react()],
   server: {
     port: 5173,
     host: true
